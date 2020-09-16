d8f66d5fc72c302aef5341cf9f0c37250456b6b9
rohit <kumbharrohit13@gmail.com>
2020-09-16 19:35:18 +0530

    main class added

---
+++ messanger-api/src/main/java/com/messanger/MessangerApp.java
@@ -0,0 +1,11 @@
+package com.messanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessangerApp {
+	public static void main(String[] args) {
+		SpringApplication.run(MessangerApp.class, args);
+	}
+}
