7e60f948476b36e70d2e42ea9fdfd4b1599ec41b
unknown <kumbharrohit13@gmail.com>
2021-01-17 12:12:51 +0530

    Updated

--- springboot-mongo-restapi-crud-demo/src/main/java/com/mongo/restapi/crud/SpringbootMongoRestapiCrudDemoApplication.java
+++ springboot-mongo-restapi-crud-demo/src/main/java/com/mongo/restapi/crud/SpringbootMongoRestapiCrudDemoApplication.java
@@ -1,4 +1,4 @@
-package com.mongo.restapi.springbootmongorestapicruddemo;
+package com.mongo.restapi.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
