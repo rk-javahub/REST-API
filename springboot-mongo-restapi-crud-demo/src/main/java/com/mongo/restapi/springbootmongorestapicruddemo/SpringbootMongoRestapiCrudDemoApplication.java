c208702e2c8e3ee330b350c0c54c78fafa75a5f6
unknown <kumbharrohit13@gmail.com>
2021-01-17 12:04:47 +0530

    Project pushed to repmote repo

---
+++ springboot-mongo-restapi-crud-demo/src/main/java/com/mongo/restapi/springbootmongorestapicruddemo/SpringbootMongoRestapiCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.restapi.springbootmongorestapicruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoRestapiCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoRestapiCrudDemoApplication.class, args);
+	}
+
+}
---
+++ springboot-mongo-restapi-crud-demo/src/test/java/com/mongo/restapi/springbootmongorestapicruddemo/SpringbootMongoRestapiCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongo.restapi.springbootmongorestapicruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongoRestapiCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
